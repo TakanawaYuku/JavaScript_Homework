8ab9ce9aa23adb978ec1a52e8141b30015117214
TakanawaYuku <yukutakanawa@gmail.com>
2022-12-10 16:43:37 +0300

    add task 1

---
+++ homework_5/task_1/js/script.js
@@ -0,0 +1,16 @@
+let timer = 59;
+
+let myTimer = setInterval(() => {
+
+	if(timer > 0) {
+		timer = timer - 1;
+		document.getElementById("timer").innerHTML = timer
+	}
+
+	if (timer == 0) {
+		clearInterval(myTimer);
+		document.getElementById("timer").innerHTML = alert("Вы победили в конкурсе");
+        // document.getElementById("timer").innerHTML = "Вы победили в конкурсе";
+	}
+
+}, 1000);
